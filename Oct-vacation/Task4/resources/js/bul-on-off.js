878269ffa8448e3916a9830c662af129d50e766b
Red-Sa1t <1129200163@qq.com>
2024-10-05 19:10:54 +0800

    Update bul-on-off.js

--- Oct-vacation/Task4/resources/js/bul-on-off.js
+++ Oct-vacation/Task4/resources/js/bul-on-off.js
@@ -1,4 +1,13 @@
 function changeImage() {
   // TODO
   // 在此处，实现小灯开关的功能！
+  element=document.getElementById('myimage')
+	if (element.src.match("bulbon"))
+	{
+		element.src="/imgs/pic_bulboff.gif";
+	}
+	else
+	{
+		element.src="/imgs/pic_bulbon.gif";
+	}
 }
